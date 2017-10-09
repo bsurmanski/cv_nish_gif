06cdf15ccc7efeec049887545f7a401e26b53212
Brandon Surmanski <b.surmanski@gmail.com>
2017-10-08 18:51:12 -0700

    simple full stitching; no focus alignment; no autocropping

--- main.py
+++ main.py
@@ -1,97 +1,120 @@
 import cv2
+import numpy as np
 import imageio
 from matplotlib import pyplot as plt
 
 POI = []
 
 class Frame:
-	def __init__(self, img):
-		self.img = img
-		self.height, self.width = img.shape[:2]
-		self.scale = 400.0 / self.height
-		self.scaled = cv2.resize(self.img, (int(self.width * self.scale), 400), 
-								 cv2.INTER_CUBIC)
-		self.scaled_h, self.scaled_w, = self.scaled.shape[:2]
-		
-	def compute(self, algo):
-		self.kp, self.des = algo.detectAndCompute(self.scaled, None)
-		
-	def cropBorder(self):
-		gray = cv2.cvtColor(self.img,cv2.COLOR_BGR2GRAY)
-		_,thresh = cv2.threshold(gray,1,255,cv2.THRESH_BINARY)
-		_,contours,hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-		cnt = contours[0]
-		x,y,w,h = cv2.boundingRect(cnt)
-		return self.img[y:y+h,x:x+w]
+    def __init__(self, img):
+        self.img = img
+        self._gray = None
+        self.height, self.width = img.shape[:2]
+        self.scale = 400.0 / self.height
+        self.scaled = cv2.resize(self.img, (int(self.width * self.scale), 400),
+                                 None, interpolation=cv2.INTER_CUBIC)
+        self.scaled_h, self.scaled_w, = self.scaled.shape[:2]
+
+    def gray(self):
+        if self._gray is None:
+            self._gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+        return self._gray
+
+    def compute(self, algo):
+        self.kp, self.des = algo.detectAndCompute(self.scaled, None)
+
+    def cropBorder(self):
+        return self.img
+        pass
+        gray = cv2.cvtColor(self.img,cv2.COLOR_BGR2GRAY)
+        _,thresh = cv2.threshold(gray,1,255,cv2.THRESH_BINARY)
+        _,contours,hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        cnt = contours[0]
+        x,y,w,h = cv2.boundingRect(cnt)
+        return self.img[y:y+h,x:x+w]
+
+
+def POI_frame_index(width, num_images):
+    print "POI ", POI[0], float(width), num_images
+    return int((POI[0] / float(width)) * num_images)
 
 
 def capture_focus(event, x, y, flags, param):
-	global POI
-	if event == cv2.EVENT_LBUTTONUP:
-		POI = [x, y]
-		
+    global POI
+    if event == cv2.EVENT_LBUTTONUP:
+            POI = [x, y]
+
 def sliceFrames(src_frame, num_images):
-	w, h = src_frame.width, src_frame.height
-	frames = [Frame(src_frame.img[0:h, i*w/4:(i+1)*w/4]) 
-			  for i in range(0, 4)]
-	return frames
-		
+    w, h = src_frame.width, src_frame.height
+    frames = [Frame(src_frame.img[0:h, i*w/4:(i+1)*w/4])
+              for i in range(0, 4)]
+    return frames
+
+def alignFrames(frames, warp_mode=cv2.MOTION_TRANSLATION):
+    poi_frame = POI_frame_index(sum([f.scaled_w for f in frames]), len(frames))
+    ret = []
+    for i in range(len(frames)):
+        print("align ", i)
+        warp_matrix = np.eye(2, 3, dtype=np.float32)
+        if i == poi_frame:
+            ret.append(frames[i])
+            continue
+        ref = frames[poi_frame]
+        cv2.findTransformECC(ref.gray(), frames[i].gray(), warp_matrix, warp_mode)
+        aligned = cv2.warpAffine(frames[i].img, warp_matrix, (ref.width, ref.height),
+                                 flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+        ret.append(Frame(aligned))
+
+    return ret
+
+
 def sliceAndAlignImages(src, num_images):
-	src_frame = Frame(src)
+    global POI
 
-	cv2.namedWindow('image')
-	cv2.setMouseCallback('image', capture_focus)
-	
-	while True:
-		img = src_frame.scaled.copy()
-		
-		# draw sample slice lines
-		for i in range(1, num_images):
-			cv2.line(img, 
-					 (i*src_frame.scaled_w/4, 0), 
-					 (i*src_frame.scaled_w/4, src_frame.scaled_h), 
-					 (0, 0, 255))
+    src_frame = Frame(src)
 
-		# draw POI
-		if POI:
-			cv2.circle(img, (POI[0], POI[1]), 
-					   radius=3, color=(0, 0, 255))
+    cv2.namedWindow('image')
+    cv2.setMouseCallback('image', capture_focus)
 
-		cv2.imshow('image', img)
-		key = cv2.waitKey(1) & 0xFF
-		if key == ord('c'):
-			break
-	
-	frames = sliceFrames(src_frame, num_images)
-	for frame in frames:
-		frame.compute(orb)
-	
-	bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-	matches = bf.match(frames[0].des, frames[1].des)
-	# Sort them in the order of their distance.
-	matches = sorted(matches, key = lambda x:x.distance)
-	img3 = cv2.drawMatches(frames[0].scaled, frames[0].kp, 
-						   frames[1].scaled, frames[1].kp, matches, None, flags=2)
-	#img2 = cv2.drawKeypoints(frame.img, frame.kp, None, color=(0,0,255), flags=0)
-	#plt.imshow(img3),plt.show()
-	
-	return frames
-	
-			
-def outputToGif(filename, frames):
-	imageio.mimsave(filename, [f.cropBorder() for f in frames], 
-					duration=0.1)
+    while True:
+        img = src_frame.scaled.copy()
+
+        # draw sample slice lines
+        for i in range(1, num_images):
+            cv2.line(img, (i*src_frame.scaled_w/4, 0),
+                     (i*src_frame.scaled_w/4, src_frame.scaled_h),
+                     (0, 0, 255))
+
+        # draw POI
+        if POI:
+            cv2.circle(img, (POI[0], POI[1]), radius=3, color=(0, 0, 255))
+
+        cv2.imshow('image', img)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('c'):
+            break
+
+    frames = sliceFrames(src_frame, num_images)
+    aligned = alignFrames(frames)
+
+    return aligned
+
+
+def outputToGif(filename, frames, boomerang=True):
+    if boomerang:
+        rev = frames[1:-1]
+        rev.reverse()
+        frames = frames[:] + rev
+    # weird index converts from BGR to RGB
+    imageio.mimsave(filename, [f.cropBorder()[...,::-1] for f in frames],
+                    duration=0.1)
 
 def main():
-	src = cv2.imread('img.jpg', cv2.IMREAD_UNCHANGED)
-	
-	frames = sliceFrames(Frame(src), 4)
-	[f.cropBorder() for f in frames]
-	#frames = sliceAndAlignImages(src, 4)
-	#outputToGif('out.gif', frames)
-	
-	cv2.destroyAllWindows()
+    src = cv2.imread('img_small.jpg', cv2.IMREAD_UNCHANGED)
+    frames = sliceAndAlignImages(src, 4)
+    outputToGif('out.gif', frames)
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-	main()
+    main()

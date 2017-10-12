8cb7cc818d12c18e6d538fc6deeeac0fafe538da
Brandon Surmanski <b.surmanski@gmail.com>
2017-10-11 22:20:47 -0700

    draw POI rectangle

--- main.py
+++ main.py
@@ -65,13 +65,19 @@ class Frame:
     
     def POI_frame_offset(self, num_frames, edge_padding=32):
       clamp = lambda low, x, high: max(low, min(x, high))
+      frame_width = int(self.width / num_frames)
       x = clamp(edge_padding,
-                POI[0] - (self.POI_frame_index(num_frames) * num_frames),
-                int(self.width / num_frames) - edge_padding)
+                POI[0] - (self.POI_frame_index(num_frames) * frame_width),
+                frame_width - edge_padding)
       y = clamp(edge_padding,
                 POI[1],
                 self.height - edge_padding)
-      return x, y
+      return int(x), int(y)
+      
+    def POI_location(self, num_frames, edge_padding=32):
+      f_index = self.POI_frame_index(num_frames)
+      off = self.POI_frame_offset(num_frames, edge_padding)
+      return ((int(self.width / (num_frames)) * f_index) + off[0], off[1])
 
 
 def CommonCrop(rects):
@@ -100,6 +106,8 @@ def sliceFrames(src_frame, num_images):
     
 def alignFrames(frames, poi_frame, poi_offset, poi_stride, warp_mode=cv2.MOTION_TRANSLATION):
     ret = []
+    poi_img = frames[poi_frame].img[int(poi_offset[0]-poi_stride/2):poi_stride,
+                                    int(poi_offset[1]-poi_stride/2):poi_stride]
     for i in range(len(frames)):
         print("align ", i)
         warp_matrix = np.eye(2, 3, dtype=np.float32)
@@ -123,29 +131,35 @@ def sliceAndAlignImages(src, num_frames):
     cv2.namedWindow('image')
     cv2.setMouseCallback('image', capture_focus)
 
+    POI_STRIDE = 64
+    POI_STRIDE_SCALED = POI_STRIDE * src_frame.scale
     while True:
-        img = src_frame.scaled.copy()
+        img = Frame(src_frame.scaled.copy())
 
         # draw sample slice lines
         for i in range(1, num_frames):
-            cv2.line(img, (i*src_frame.scaled_w/4, 0),
+            cv2.line(img.img, (i*src_frame.scaled_w/4, 0),
                      (i*src_frame.scaled_w/4, src_frame.scaled_h),
                      (0, 0, 255))
 
         # draw POI
         if POI:
-            cv2.circle(img, (POI[0], POI[1]), radius=3, color=(0, 0, 255))
+            cv2.circle(img.img, (POI[0], POI[1]), radius=3, color=(0, 0, 255))
+            loc = img.POI_location(num_frames, edge_padding=POI_STRIDE_SCALED)
+            cv2.rectangle(img.img, 
+                          (loc[0] - int(POI_STRIDE_SCALED/2), loc[1] - int(POI_STRIDE_SCALED/2)),
+                          (loc[0] + int(POI_STRIDE_SCALED/2), loc[1] + int(POI_STRIDE_SCALED/2)), color = (0, 0, 255))
+            
 
-        cv2.imshow('image', img)
+        cv2.imshow('image', img.img)
         key = cv2.waitKey(1) & 0xFF
         if key == ord('c'):
             break
 
-    poi_stride = 64
     poi_frame = src_frame.POI_frame_index(num_frames)
-    poi_offset = src_frame.POI_frame_offset(num_frames, padding=int(poi_stride/2))
+    poi_offset = src_frame.POI_frame_offset(num_frames, edge_padding=POI_STRIDE)
     frames = sliceFrames(src_frame, num_frames)
-    aligned = alignFrames(frames, poi_frame, poi_offset, poi_stride)
+    aligned = alignFrames(frames, poi_frame, poi_offset, POI_STRIDE)
 
     bounding_rects = [a.findCrop() for a in aligned]
     crop_rect = CommonCrop(bounding_rects)
